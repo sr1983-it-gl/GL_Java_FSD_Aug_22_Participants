a56ee103a98fd69568e7f8a4d84bb432279d0052
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-01-08 15:26:34 +0530

    Lab06 - EMS - Welcome Page

---
+++ 018_08_Jan_2023_Lab_05/EMS/src/main/java/com/greatlearning/ems/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.greatlearning.ems.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/welcome")
+	public String showWelcomePage() {
+		return "welcome-page";
+	}
+}
